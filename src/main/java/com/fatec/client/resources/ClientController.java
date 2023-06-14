1468ba7283e39e107020b2c05c20109a902ed8e6
josusz <josuhcarvalho@gmail.com>
2023-06-14 12:33:00 -0300

    correção no editClient

--- src/main/java/com/fatec/client/resources/ClientController.java
+++ src/main/java/com/fatec/client/resources/ClientController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class ClientController {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("{id}")
+    @PutMapping("{id}")
     public ResponseEntity<Client> editClient(@PathVariable int id, @RequestBody Client client) {
         clientService.update(id, client);
         return ResponseEntity.ok().build();
